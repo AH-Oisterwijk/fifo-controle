ae0054f472248b7c2d3bb6861eff2a328abedf95
Bob ter Schegget <bob.ter.schegget@gmail.com>
2026-07-15 22:26:24 +0200

    Third attempt to fix wrong redirect.

--- config.js
+++ config.js
@@ -1,13 +1,8 @@
 // Microsoft Forms-koppeling.
-//
-// De HTML vult de Forms-link via de exacte veld-id's.
-// Daardoor ontstaan er geen problemen meer met placeholders, dubbele URL-encoding
-// of waarden zoals [object Object].
 
 window.FIFO_FORMS_BASE_URL = "https://forms.office.com/Pages/ResponsePage.aspx?id=8WmxpitZKUOPM424kDADx2Cr8gJsc3VFkMYYzm7FzuxUME5TM0MzV0ZSUklUQlQ5QlNMQjNNSVVIRS4u";
 
 window.FIFO_FORMS_DATE_FIELD = "r17f3b67a5f6a493296cd4014b1750afc";
 window.FIFO_FORMS_DATA_FIELD = "r10511d40c504460fa25e4e72bce0c561";
 
-// Laat leeg. Alleen nog aanwezig als fallback voor oude configuraties.
 window.FIFO_FORMS_URL_TEMPLATE = "";
