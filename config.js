f58d299e6fc0d17c437ae0993eeeddc4fa6dfbdd
Bob ter Schegget <bob.ter.schegget@gmail.com>
2026-07-15 22:35:17 +0200

    Fourth attempt to fix wrong redirect.

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-// Microsoft Forms-koppeling.
+// Microsoft Forms prefilled link.
+//
+// Vervang in de prefilled link:
+// - de datumwaarde, bijvoorbeeld %222026-01-02%22, door __DATUM__
+// - de tekstvakwaarde, bijvoorbeeld Data, door __CONTROLE_DATA__
 
-window.FIFO_FORMS_BASE_URL = "https://forms.office.com/Pages/ResponsePage.aspx?id=8WmxpitZKUOPM424kDADx2Cr8gJsc3VFkMYYzm7FzuxUME5TM0MzV0ZSUklUQlQ5QlNMQjNNSVVIRS4u";
-
-window.FIFO_FORMS_DATE_FIELD = "r17f3b67a5f6a493296cd4014b1750afc";
-window.FIFO_FORMS_DATA_FIELD = "r10511d40c504460fa25e4e72bce0c561";
-
-window.FIFO_FORMS_URL_TEMPLATE = "";
+window.FIFO_FORMS_URL_TEMPLATE = "https://forms.office.com/Pages/ResponsePage.aspx?id=8WmxpitZKUOPM424kDADx2Cr8gJsc3VFkMYYzm7FzuxUME5TM0MzV0ZSUklUQlQ5QlNMQjNNSVVIRS4u&r17f3b67a5f6a493296cd4014b1750afc=__DATUM__&r10511d40c504460fa25e4e72bce0c561=__CONTROLE_DATA__";
