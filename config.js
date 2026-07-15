bf01642f70545fd8990efb2ac159d9170b53175b
Bob ter Schegget <bob.ter.schegget@gmail.com>
2026-07-15 21:52:29 +0200

    Fixed bad redirect.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // - Datum
 // - Controle data
 //
-// Placeholders in de prefilled link:
+// Gebruik deze placeholders:
 // __DATUM__
 // __CONTROLE_DATA__
 window.FIFO_FORMS_URL_TEMPLATE = "https://forms.office.com/Pages/ResponsePage.aspx?id=8WmxpitZKUOPM424kDADx2Cr8gJsc3VFkMYYzm7FzuxUME5TM0MzV0ZSUklUQlQ5QlNMQjNNSVVIRS4u&r17f3b67a5f6a493296cd4014b1750afc=__DATUM__&r10511d40c504460fa25e4e72bce0c561=__CONTROLE_DATA__";
